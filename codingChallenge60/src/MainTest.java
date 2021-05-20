d46bb7f5a342f3cbdf263529e452897e55132fc6
Eskor Eyo <eskor.eyo@jamf.com>
2021-05-19 21:02:49 -0500

    Created 2nd unit test

--- codingChallenge60/src/MainTest.java
+++ codingChallenge60/src/MainTest.java
@@ -1,14 +1,28 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
+    Main m;
+    @BeforeEach
+    public void init() {
+        m = new Main();
+    }
+
+
     @Test
     public void testGetDurationString() {
-        Main m = new Main();
         String test1 = m.getDurationString(0, 66);
         assertEquals("invalid value", test1);
     }
 
+    @Test
+    public void testGetDurationString2() {
+        String test1 = m.getDurationString();
+        assertEquals("invalid value", test1);
+
+    }
 }
+
