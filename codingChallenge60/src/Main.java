f3937a486e9e03061f27b2ffd131f843f16b3f8d
Eskor Eyo <eskor.eyo@jamf.com>
2021-05-21 16:19:28 -0500

    Created 2nd function on 2nd test

--- codingChallenge60/src/Main.java
+++ codingChallenge60/src/Main.java
@@ -1,9 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(getDurationString(12, 29));
     }
     public static String getDurationString(int minutes, int seconds) {
         if (minutes >= 0 && seconds >=0 && seconds <= 59) {
+            return "valid number";
+        }
+        return "invalid value";
+    }
+
+    public static String getDurationString(int minutes) {
+        if (minutes >= 0) {
             return "valid number";
         }
         return "invalid value";
