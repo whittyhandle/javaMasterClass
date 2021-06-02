b3a4ddf047bb6c391896fdaf38d14d82afe8e7e9
whittyhandle <kittledavid@gmail.com>
2021-06-02 17:24:02 -0500

    testing getters and setters with java reflection api

--- CodingChallenge/src/CodingExercise30/PersonTest.java
+++ CodingChallenge/src/CodingExercise30/PersonTest.java
@@ -20,33 +20,51 @@ class PersonTest {
     void getFirstName() throws NoSuchFieldException, IllegalAccessException {
         //given a person object has been setup
         //when we set a value
-        person.setFirstName("Jon");
         //then (using reflection) get a instance of the class and the field
         final Field field = person.getClass().getDeclaredField("firstName");
         // make the private accessible
         field.setAccessible(true);
+        field.set(person, "Jon");
+        String result = person.getFirstName();
         // check if the expected value matches the field's value
-        assertEquals("Jon", field.get(person), "Fields didn't match");
+        assertEquals("Jon", result, "Fields didn't match");
+    }
+
+    @Test
+    void getAge() throws NoSuchFieldException, IllegalAccessException {
+        //given a person object has been setup
+        //when we set a value
+        //then (using reflection) get a instance of the class and the field
+        final Field field = person.getClass().getDeclaredField("age");
+        // make the private accessible
+        field.setAccessible(true);
+        field.set(person, 1);
+        int result = person.getAge();
+        //then
+        assertEquals(1, result, "Fields didn't match");
+
+    }
+
+    @Test
+    void setFirstName()  throws NoSuchFieldException, IllegalAccessException {
+        //given a person object has been setup
+        //when we set a value
+        person.setFirstName("Mary");
+        final Field field = person.getClass().getDeclaredField("firstName");
+        field.setAccessible(true);
+        assertEquals("Mary", field.get(person), "Fields didn't match");
     }
 
 
     @Test
-    void setFirstName() {
-    }
+    void setAge() throws NoSuchFieldException, IllegalAccessException {
+        //given a person object has been setup
+        //when we set a value
+        person.setAge(1);
+        final Field field = person.getClass().getDeclaredField("age");
+        field.setAccessible(true);
+        assertEquals(1, field.get(person), "Fields didn't match");
 
-    @Test
-    void getLastName() {
-    }
 
-    @Test
-    void setLastName() {
-    }
-
-    @Test
-    void getAge() {
-    }
-
-    @Test
-    void setAge() {
     }
 }
