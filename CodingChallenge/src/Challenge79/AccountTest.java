9d30ef58e6838c66cb6d974db9257032ac5a6ecc
whittyhandle <kittledavid@gmail.com>
2021-06-07 17:38:00 -0500

    wrote unit tests and fixed withdrawl method

--- CodingChallenge/src/Challenge79/AccountTest.java
+++ CodingChallenge/src/Challenge79/AccountTest.java
@@ -1,16 +1,36 @@
 package Challenge79;
 
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTest {
 
-    //todo dk/Eskor write tests
+    Account account;
 
-    @org.junit.jupiter.api.Test
-    void deposit() {
+    @BeforeEach
+    public void init(){
+        account = new Account();
+
     }
 
     @org.junit.jupiter.api.Test
-    void withdrawal() {
+    void testDepositPass() {
+        int amount = 20;
+        double actual = account.deposit(amount);
+        double expect = account.getBalance();
+        assertEquals(expect, actual, "values do not match");
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdrawalPass() {
+        int amount = 20;
+        double balance = account.setBalance(20);
+        double withdrawl = account.withdrawal(20);
+        double actual = account.getBalance();
+        double expect = 0;
+        assertEquals(expect, actual, "values don't match");
+
+
     }
 }
