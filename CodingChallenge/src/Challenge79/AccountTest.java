ff1538390be7912472035c0d9da84060824e0468
whittyhandle <kittledavid@gmail.com>
2021-06-06 20:39:44 -0500

    start of test

---
+++ CodingChallenge/src/Challenge79/AccountTest.java
@@ -0,0 +1,16 @@
+package Challenge79;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    //todo dk/Eskor write tests
+
+    @org.junit.jupiter.api.Test
+    void deposit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdrawal() {
+    }
+}
