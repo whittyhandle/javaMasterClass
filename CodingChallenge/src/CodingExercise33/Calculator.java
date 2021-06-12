39fd3d7d4d40cd86c649964cd415c8aa3a9f6849
Robert Sanford <rsanford7@student.cvtc.edu>
2021-06-11 21:15:11 -0500

    basic framework for classes and Calculator test

--- CodingChallenge/src/CodingExercise33/Calculator.java
+++ CodingChallenge/src/CodingExercise33/Calculator.java
@@ -1,5 +1,15 @@
 package CodingExercise33;
 
 public class Calculator {
-    //todo rob
+
+    // Declare instance variables
+    // Floor floor
+    // Carpet carpet
+
+    // Constructor with two parameters (Floor floor, Carpet carpet)
+        // initialize them
+
+    // Method named getTotalCost(no parameters)
+        // Return calculated cost getArea() * getCost()
+
 }
--- CodingChallenge/src/CodingExercise33/Floor.java
+++ CodingChallenge/src/CodingExercise33/Floor.java
@@ -1,5 +1,20 @@
 package CodingExercise33;
 
 public class Floor {
-    //todo rob, dk, eskor
+    // Declare variables
+    // double width
+    // double length
+
+    // Constructor, two param's (width, length)
+
+        // statement checking if value width < 0
+            // if < 0 then width = 0
+            // else instantiate width
+
+        // statement checking if value length < 0
+            // if < 0 then length = 0
+            // else instantiate length
+
+    // Method named getArea(no param's)
+        // Return width * length
 }
